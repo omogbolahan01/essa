83898f2e18825a7320f1a86c79603cb9de489b24
omogbolahan01 <iloriojommodel@gmail.com>
2023-11-23 08:59:30 +0100

    iii

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,7 @@ export default function Footer() {
           </div>
           <div className="nav-bottom">
             <NavLink
-              to="features"
+              to="/"
               style={({ isActive }) => (isActive ? activeStyles : null)}
             >
               Features
